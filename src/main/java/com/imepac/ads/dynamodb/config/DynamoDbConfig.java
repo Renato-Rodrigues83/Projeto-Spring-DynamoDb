67681112ea0aa399cecdafff3623c5405614e6fc
Renato-Rodrigues83 <renatinprado8832@gmail.com>
2024-09-30 09:36:42 -0300

    terminando api

--- src/main/java/com/imepac/ads/dynamodb/config/DynamoDbConfig.java
+++ src/main/java/com/imepac/ads/dynamodb/config/DynamoDbConfig.java
@@ -1,27 +1,41 @@
 package com.imepac.ads.dynamodb.config;
 
+import com.imepac.ads.dynamodb.entities.NotasAlunosEntity;
+import io.awspring.cloud.dynamodb.DynamoDbTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
 import java.net.URI;
 
 @Configuration
 public class DynamoDbConfig {
 
+    @Value("${aws.dynamodb.endpoint}")
+    private String dynamoDbEndpoint;
+
+    @Value("${aws.dynamodb.region}")
+    private String dynamoDbRegion;
+
+    @Value("${aws.dynamodb.access-key}")
+    private String dynamoDbAccessKey;
+
+    @Value("${aws.dynamodb.secret-key}")
+    private String dynamoDbSecretKey;
+
     @Bean
     public DynamoDbClient dynamoDbClient() {
         return DynamoDbClient.builder()
-                .region(Region.US_EAST_1)
-                .endpointOverride(URI.create("http://localhost:4566")) // LocalStack DynamoDB endpoint
-                .build();
-    }
-
-    @Bean
-    public DynamoDbEnhancedClient dynamoDbEnhancedClient(DynamoDbClient dynamoDbClient) {
-        return DynamoDbEnhancedClient.builder()
-                .dynamoDbClient(dynamoDbClient)
+                .region(Region.of(dynamoDbRegion))
+                .endpointOverride(URI.create(dynamoDbEndpoint))
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(dynamoDbAccessKey, dynamoDbSecretKey)))
                 .build();
     }
 }
--- src/main/java/com/imepac/ads/dynamodb/controller/NotaController.java
+++ src/main/java/com/imepac/ads/dynamodb/controller/NotaController.java
@@ -1,39 +1,65 @@
 package com.imepac.ads.dynamodb.controller;
 
-//import com.imepac.ads.dynamodb.DTO.NotaDTO;
+import com.imepac.ads.dynamodb.DTO.NotaDTO;
 import com.imepac.ads.dynamodb.entities.NotasAlunosEntity;
-import com.imepac.ads.dynamodb.repository.NotaRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.awspring.cloud.dynamodb.DynamoDbTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import software.amazon.awssdk.enhanced.dynamodb.Key;
+import software.amazon.awssdk.enhanced.dynamodb.model.PageIterable;
+import software.amazon.awssdk.enhanced.dynamodb.model.QueryConditional;
+import software.amazon.awssdk.enhanced.dynamodb.model.QueryEnhancedRequest;
 
-
-
-//import io.awspring.cloud.dynamodb.DynamoDbTemplate;
-//import software.amazon.awssdk.enhanced.dynamodb.Key;
-//import software.amazon.awssdk.enhanced.dynamodb.model.PageIterable;
-//import software.amazon.awssdk.enhanced.dynamodb.model.QueryConditional;
-//import software.amazon.awssdk.enhanced.dynamodb.model.QueryEnhancedRequest;
-
-//import java.util.List;
-
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import java.util.List;
 
 @RestController
 @RequestMapping("v1/notas")
 public class NotaController {
 
-    @Autowired
-    private NotaRepository notaRepository;
+    private final DynamoDbTemplate dynamoDbTemplate;
 
-    @PostMapping
-    public String createPessoa (@RequestBody NotasAlunosEntity notasAlunosEntity) {
-        notaRepository.save(notasAlunosEntity);
-        return "Nota salva com sucesso";
+    private static final Logger logger = LoggerFactory.getLogger(NotaController.class);
+
+    public NotaController(DynamoDbTemplate dynamoDbTemplate) {
+        this.dynamoDbTemplate = dynamoDbTemplate;
+    }
+
+    @PostMapping("/{matricula}/{idDisciplina}")
+    public ResponseEntity<Void> salvar(
+            @PathVariable("matricula") String matricula,
+            @PathVariable("idDisciplina") String idDisciplina,
+            @RequestBody NotaDTO nota) {
+
+        var entity = NotasAlunosEntity.fromNotaDTO(matricula, idDisciplina, nota);
+        dynamoDbTemplate.save(entity);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/matricula/{matricula}")
+    public ResponseEntity<List<NotasAlunosEntity>> buscarNotasPorAluno(@PathVariable("matricula") String matricula) {
+        Key key = Key.builder()
+                .partitionValue(matricula)
+                .sortValue("NOTA#")
+                .build();
+
+        QueryConditional notas = QueryConditional.sortBeginsWith(key);
+
+        QueryEnhancedRequest query = QueryEnhancedRequest
+                .builder()
+                .queryConditional(notas)
+                .build();
+
+        try {
+            PageIterable<NotasAlunosEntity> nota = dynamoDbTemplate.query(query, NotasAlunosEntity.class);
+
+            List<NotasAlunosEntity> notaList = nota.items().stream().toList();
+
+            return ResponseEntity.ok(notaList);
+
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
